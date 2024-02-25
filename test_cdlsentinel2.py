e0e543e9f866e26bd618ec1cb8c594294244dabf
Yi-Chia Chang <yichia3@illinois.edu>
2024-02-24 21:15:36 -0600

    update

--- test_cdlsentinel2.py
+++ test_cdlsentinel2.py
@@ -1,24 +1,2 @@
 from torchgeo.datasets import Sentinel2
-from torchgeo.datasets import CDL
-
 Sentinel2(paths="/projects/dali/data/sentinel2/")
-CDL(paths="/projects/dali/data/cdl/", download=True, years=[2021,2022,2023])
-
-
-from torchgeo.datamodules import CDLSentinel2DataModule
-
-datamodule = CDLSentinel2DataModule(batch_size=64, patch_size=16)
-task = SemanticSegmentationTask(
-    loss="ce",
-    model="unet",
-    backbone="resnet50",
-    weights=True,
-    in_channels=13,
-    num_classes=134,
-    ignore_index=0,
-    lr=0.1,
-    patience=6,
-)
-
-trainer = Trainer(default_root_dir="...")
-trainer.fit(model=task, datamodule=datamodule)
