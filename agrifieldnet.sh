6d2fcb0bbac88a12d936b32fc138d81d588d82a3
Yi-Chia Chang <yichia3@illinois.edu>
2024-03-26 14:40:45 -0500

    agrifieldnet exp

---
+++ agrifieldnet.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=48:00:00
+#SBATCH --mem=128G
+#SBATCH --job-name=agri-imagenet
+
+#SBATCH --partition=gpuA100x4
+#SBATCH --gpus-per-node=1
+#SBATCH --account=bcnh-delta-gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+
+#SBATCH --mail-user=yichia3@illinois.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --output=%x-%j.out
+
+. /projects/bcnh/spack/share/spack/setup-env.sh
+spack env activate dali
+
+cd ~/torchgeo
+python3 -m torchgeo fit --config experiments/torchgeo/conf/agrifieldnet.yaml
