cd4036b223bc922dac0aceb4b347db9c889617fb
Yi-Chia Chang <yichia3@illinois.edu>
2025-09-03 09:33:42 -0500

    add collate_fn_timeseries to utils.py

--- torchgeo/datamodules/pastis.py
+++ torchgeo/datamodules/pastis.py
@@ -7,50 +7,11 @@ from typing import Any
 
 import kornia.augmentation as K
 import torch
-from torch import Tensor
 from torch.utils.data import random_split
 
 from ..datasets import PASTIS
 from .geo import NonGeoDataModule
-
-
-def collate_fn(
-    batch: list[dict[str, Tensor]], max_timestamp: int = 61
-) -> dict[str, Any]:
-    """Custom timeseries collate fn to handle variable length sequences.
-
-    Args:
-        batch: list of sample dicts return by dataset
-        max_timestamp: maximum length of the time series
-
-    Returns:
-        batch dict output
-
-    .. versionadded:: 0.8
-    """
-    output: dict[str, Any] = {}
-    images = [sample['image'] for sample in batch]
-
-    padded_images = []
-    for img in images:
-        if img.shape[0] < max_timestamp:
-            # padding_shape = (max_timestamp - img.shape[0],) + img.shape[1:]
-            padding_shape = (max_timestamp - img.shape[0], *img.shape[1:])
-            padding = torch.zeros(padding_shape, dtype=img.dtype)
-            padded_img = torch.cat([img, padding], dim=0)
-            padded_images.append(padded_img)
-        else:
-            padded_images.append(img)
-
-    output['image'] = torch.stack(padded_images)
-    output['mask'] = torch.stack([sample['mask'] for sample in batch])
-
-    if 'bbox_xyxy' in batch[0]:
-        output['bbox_xyxy'] = torch.stack([sample['bbox_xyxy'] for sample in batch])
-    if 'label' in batch[0]:
-        output['label'] = torch.stack([sample['label'] for sample in batch])
-
-    return output
+from .utils import collate_fn_timeseries
 
 
 class PASTISDataModule(NonGeoDataModule):
@@ -83,7 +44,7 @@ class PASTISDataModule(NonGeoDataModule):
             PASTIS, batch_size=batch_size, num_workers=num_workers, **kwargs
         )
         self.max_timestamp = max_timestamp
-        self.collate_fn = lambda batch: collate_fn(
+        self.collate_fn = lambda batch: collate_fn_timeseries(
             batch, max_timestamp=self.max_timestamp
         )
         self.val_split_pct = val_split_pct
